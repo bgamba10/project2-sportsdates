f93b4553deb3006c183364661b6d387f4f21feb8
Bibiana <bibianagambas@Bibianas-MacBook-Pro.local>
2018-03-03 15:17:32 -0500

    app changed

--- myapp/frontend/src/App.js
+++ myapp/frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import DatesList from "./DatesList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <DatesList>
--- myapp/frontend/src/DatesList.js
+++ myapp/frontend/src/DatesList.js
@@ -3,15 +3,19 @@ import './index.css';
 
 class DatesList extends Component {
   render() {
+
+    const { title } = this.props;
+    console.log(this.props);
+
     return (
       <div className="App">
           <h1 className="App-title">Welcome to DatesList</h1>
-          <h4>{title}</h4>
+          <p className="App-title">holaaaaaa</p>
+          <h4 className="App-title">{title}</h4>
       </div>
     );
   }
 }
-
-///export default DatesList;
+export default DatesList;
 
 
