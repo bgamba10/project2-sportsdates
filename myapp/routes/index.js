3b2e9c9ef74824e34ef50b49d1734c26bff2589c
nacevedo <n.acevedos@uniandes.edu.co>
2018-03-05 00:10:36 -0500

    New member working

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -33,6 +33,17 @@ const insertDocuments = function(db,d,callback) {
 });
 }
 
+const removeDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });    
+}
 
 
 function getFollowers(callback) {
