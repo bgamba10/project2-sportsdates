0cdd2532134b48b47906c48d95a3957a92303f09
nacevedo <n.acevedos@uniandes.edu.co>
2018-03-04 21:50:35 -0500

    New register button and style chenges

--- myapp/frontend/src/components/Dates.js
+++ myapp/frontend/src/components/Dates.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import request from "../../node_modules/superagent/superagent";
+import './newdate.css';
 
 export default class Dates extends React.Component {
 
@@ -7,36 +8,64 @@ export default class Dates extends React.Component {
     super(props);
 
     this.state={
+  
+       _id: {
+        "$oid": null
+      },
       sport: null,
       numPeople: null,
       place:null,
       comment:null,
-      invite:null, 
-      registered:[]
+      invite:null,
+      reg:['hola'],
+      
     };
+
+    this.register = this.register.bind(this);
   }
 
   register(event)
   {
     console.log("Registradose"); 
+    console.log(this.props);
+
+    request
+    .post('/api')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send({ 
+        _id: this.props.id,
+        sport: this.props.sport, 
+        numPeople: this.props.numPeople,
+        place: this.props.place,
+        comment: this.props.comment,
+        invite: this.props.invite,
+        reg: this.props.reg.push("nuevo")
+    })
+    .end(function(err, res){
+        console.log(res.text);
+    });
   }
 
 
   render() {
   	return (
-  	<div>
-  		<p>Sport: {this.props.sport}</p>
-  		<p>Number of people: {this.props.numPeople}</p>
-  		<p>Place: {this.props.place}</p>
-  		<p>Comment: {this.props.comment}</p>
-  		<p>Invite: {this.props.invite}</p>
-      <p>The people registered are:  </p> 
+  	<div className="everything">
+    <div className="col-sm-6">
+      <p>{this.props.id}</p>
+  		<p><span>Sport:</span> {this.props.sport}</p>
+  		<p><span>Number of people:</span> {this.props.numPeople}</p>
+  		<p><span>Place:</span> {this.props.place}</p>
+  		<p><span>Comment: </span>{this.props.comment}</p>
+  		<p><span>Invite: </span>{this.props.invite}</p>
+      <p><span>The people registered are: </span> </p> 
+      {console.log(this.props)}
       <p>
-            {this.state.registered.map((d,index) => {
+            {this.props.reg.map((d,index) => {
               return <span> {d} </span>
               })}
       </p>
       <button onClick={this.register}>Register here!</button>
+    </div>  
     </div>
    );
   }
--- myapp/frontend/src/components/Profile.js
+++ myapp/frontend/src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import './newdate.css';
 
 class Profile extends Component {
 
@@ -23,9 +23,9 @@ render() {
     return (
     	<div className="everything">
       <h1>{this.state.name}</h1>
-      <p> Age:{this.state.age}</p>
-      <p> Mail:{this.state.age}</p>
-      <p> Gender:{this.state.age}</p>
+      <p><span> Age: </span>{this.state.age}</p>
+      <p><span> Mail:</span>{this.state.mail}</p>
+      <p><span> Gender:</span>{this.state.gender}</p>
       </div>
       );
 }
